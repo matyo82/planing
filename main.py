80647b2a92b73436d1664fbe48347ec706341c09
matyo82 <matingarshasbii@gmail.com>
2023-06-29 02:07:13 +0330

    delete command something

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from show import show
 command = input("inter a command:")
 if command == "add":
     import_table()
-elif command == "delete" or command=="done":
-    print("delete")
 elif command == "show":
     data=input("inter datatime (Example jun,29,2023):")
     show(data)
